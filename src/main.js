9b11518bf6b8b7614f67e5e71e6fa61557f1fa63
张展亮 <zzlwte@email.com>
2017-10-31 15:56:17 +0800

    test

--- src/main.js
+++ src/main.js
@@ -17,8 +17,5 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  // el: '#app',
   router,
-  // template: '<App/>',
-  // components: { App }
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import App from '../App'
 import {routerMode} from '../config/env'
 
@@ -14,11 +13,6 @@ export default new Router({
       path: '/',
       component: App, //顶层路由，对应index.html
       children: [//二级路由。对应App.vue
-        //地址为空时跳转home页面
-        {
-          path: '',
-          redirect: '/home'
-        },
         {
           path: '/home',
           component: home
