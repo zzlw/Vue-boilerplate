f475dd589533ef5ffa55d89d4d24a91b0c5d4df0
张展亮 <zzlwte@email.com>
2017-10-31 16:52:03 +0800

    优化自动补全后戳名，添加mixin rem方法

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -20,7 +20,14 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions : [
+      '',
+      '.js',
+      '.vue',
+      '.less',
+      '.css',
+      '.scss'
+    ],
     alias: {
       // 'vue$': 'vue/dist/vue.esm.js',
       // '@': resolve('src'),
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div>sdfssdfdsfdf</div>
     <router-view/>
-    是否水电费胜多负少的
   </div>
 </template>
 
@@ -13,12 +11,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/common';
+
 </style>
